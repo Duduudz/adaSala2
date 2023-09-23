f84dcf7354f533863b7e07806d9ca181bdd34bb7
Gustavo <sasaeucomvoce@hotmail.com>
2023-09-23 16:44:45 -0300

    refactor: refactor do nome da classe

---
+++ desafio-sala2/desafio/src/main/java/com/ada/desafio/entities/CustomeFeedback.java
@@ -0,0 +1,28 @@
+package com.ada.desafio.entities;
+
+import com.ada.desafio.entities.enums.FeedbackStatusEnum;
+import com.ada.desafio.entities.enums.FeedbackTypeEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="Feedbacks")
+@NoArgsConstructor
+@Getter
+@Setter
+public class CustomeFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String type;
+    private String message;
+    private String status;
+
+    public CustomeFeedback(FeedbackTypeEnum type, FeedbackStatusEnum status, String message) {
+        this.type = String.valueOf(type);
+        this.status = String.valueOf(status);
+        this.message = message;
+    }
+}
